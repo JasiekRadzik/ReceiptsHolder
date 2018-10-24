d10f935dacdc35cc45c32c1471a4d11ab18cfae3
Radzik <radzikjasiek@gmail.com>
2018-10-24 12:37:39 +0200

    Structure

---
+++ ReceiptsHolder/Controllers/GeneralViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralViewController.swift
+//  ReceiptsHolder
+//
+//  Created by Jan Radzikowski on 24/10/2018.
+//  Copyright © 2018 Jan Radzikowski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GeneralViewController: UIViewController {
+    
+}
