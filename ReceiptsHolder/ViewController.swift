6036cb0079fc66ae634be24b9c965704102ab06e
Radzik <radzikjasiek@gmail.com>
2018-10-24 11:26:00 +0200

    Initial commit

---
+++ ReceiptsHolder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceiptsHolder
+//
+//  Created by Jan Radzikowski on 24/10/2018.
+//  Copyright © 2018 Jan Radzikowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
